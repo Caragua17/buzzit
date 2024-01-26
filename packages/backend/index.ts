30510987078957f794b33d073c01b3672c8066ec
Thomas Contini <thomas.contini@icloud.com>
2024-01-26 23:59:20 +0100

    Add ESLint and Prettier

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+const server = Bun.serve<string>({
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed :(", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      console.log(message);
+      ws.send("Je t'ai vu");
+    },
+    open(ws) {
+      console.log("new socket", ws);
+    },
+  }, // handlers
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
--- packages/frontend/index.ts
+++ packages/frontend/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+const socket = new WebSocket("ws://localhost:3000");
+
+// message is received
+socket.addEventListener("message", (event) => {
+  console.log(event);
+});
+
+// socket opened
+socket.addEventListener("open", (event) => {
+  socket.send("test");
+  console.log(event);
+});
+
+// socket closed
+socket.addEventListener("close", (event) => {
+  console.log(event);
+});
+
+// error handler
+socket.addEventListener("error", (event) => {
+  console.log(event);
+});
